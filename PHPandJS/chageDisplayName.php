a8e2e62ec31e8c3b07d0d859d6119b141c4453d3
Siberiax <agm6856@cs.rit.edu>
2017-04-20 14:57:30 -0400

    Updated php

--- PHPandJS/chageDisplayName.php
+++ PHPandJS/chageDisplayName.php
@@ -1,47 +1,36 @@
 <?php
 include_once("common.php");
-session_start();
-$has_session = session_status() == PHP_SESSION_ACTIVE;
-if($has_session){
-	$destroy = false;
-	if (!isset($_SESSION['login']) or !isset($_SESSION['user_id'])){
-		session_regenerate_id(true);
-		session_destroy();
-	}
-	if($_SERVER['REMOTE_ADDR'] !== $_SESSION['login']['ip']){
-		$destroy = true;
-	}
-	if($_SESSION['login']['born'] < time() - 300){
-		$destroy = true;
-	}
-	if($_SESSION['login']["valid"] !== true){
-		$destroy = true;
-	}
-	if($destroy===true){
-		session_destroy();
-	}
-	// Reset our counter
-	$_SESSION['login']['born'] = time();
-	$id_to_get = $_SESSION['user_id'];
-	print_r($_POST);
-	if(isset($_POST['displayName'])){
-		$prep = "UPDATE user SET displayName=? WHERE id=?";
-		if($stmt = $mysqli->prepare($prep)){
-			if($stmt->bind_param("si",$_POST['displayName'], $_SESSION['user_id'])){
-				if(!$stmt->execute()){
-					die("Error - Issue executing prepared statement: " . mysqli_error($mysqli));
-				}
-			}else{
-				die("Error - Issue binding prepared statement: " . mysqli_error($mysqli));
-			}
-			if($stmt->close()){
-				echo "Value updated succesfully";
-			}else{
-				die("Error - Failed to close prepared statement" . mysqli_error($mysqli));
+
+if (!isset($_COOKIE["JSESSIONID"]) or !isset($_SESSION['userID'])){
+	echo "Invalid Session";
+}
+
+//ip needs to be changed
+$url = "http://localhost/isAuthenticated?sessionId=" . $_COOKIE["JSESSIONID"];
+$contents = file_get_contents($url);
+if (strpos($contents, '200') == false){
+  echo 'Invalid Session';
+}
+
+$id_to_get = $_SESSION['userID'];
+print_r($_POST);
+if(isset($_POST['displayName'])){
+	$prep = "UPDATE user SET displayName=? WHERE id=?";
+	if($stmt = $mysqli->prepare($prep)){
+		if($stmt->bind_param("si",$_POST['displayName'], $_SESSION['user_id'])){
+			if(!$stmt->execute()){
+				die("Error - Issue executing prepared statement: " . mysqli_error($mysqli));
 			}
 		}else{
-			die("Error - Issue preparing statement: " . mysqli_error($mysqli));
+			die("Error - Issue binding prepared statement: " . mysqli_error($mysqli));
 		}
+		if($stmt->close()){
+			echo "Value updated succesfully";
+		}else{
+			die("Error - Failed to close prepared statement" . mysqli_error($mysqli));
+		}
+	}else{
+		die("Error - Issue preparing statement: " . mysqli_error($mysqli));
 	}
 }
 ?>
